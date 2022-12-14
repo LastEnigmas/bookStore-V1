881bbd4b792cca59143d85b93383b7715e105af1
Mehrshad <lastenigmas@gmail.com>
2022-12-14 16:51:46 +0330

    Add project files.

---
+++ bookStoreV1/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bookStoreV1.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public bool IsExist { get; set; }
+        public List<string> Tags { get; set; }
+        public int Discount { get; set; }
+        [Required]
+        public bool IsDiscount { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        [Required]
+        public string PubTitle { get; set; }
+    }
+}
